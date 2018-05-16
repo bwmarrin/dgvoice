13f6dd8287b1a88a341e9d6c1e35f154817465c0
Korvyr <viusing@gmail.com>
2018-05-16 01:21:03 -0500

    small change to allow stop <- false
    This will allow for a little select{} magic to check if something is still playing without killing the current playback:
    ```
    select{
    case stop <- false:
       // still alive and didn't kill
    case stop <- true:
       // was alive, but just killed
    default:
       // was already dead
    }
    ```

--- dgvoice.go
+++ dgvoice.go
@@ -160,8 +160,12 @@ func PlayAudioFile(v *discordgo.VoiceConnection, filename string, stop <-chan bo
 	}
 
 	go func() {
-		<-stop
-		err = run.Process.Kill()
+		for {
+			kill := <-stop
+			if kill {
+				err = run.Process.Kill()
+			}
+		}
 	}()
 
 	// Send "speaking" packet over the voice websocket
